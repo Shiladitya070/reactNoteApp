c854554f24a0d31ba7fc55e2b5ed77f2de1c3f0e
shiladitya <shiladityad940@gmail.com>
2023-08-01 23:16:49 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,49 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
+import { Route, Routes } from "react-router-dom";
+import NewNote from "./pages/NewNote";
+import { useLocalStorage } from "./customHooks/useLocalStorage";
+
+export type Note = {
+  id: string;
+} & NoteData;
+
+export type RawNote = {
+  id: string;
+};
+export type RawNoteDate = {
+  title: string;
+  markdown: string;
+  tagIds: string[];
+};
+
+export type NoteData = {
+  title: string;
+  markdown: string;
+  tags: Tag[];
+};
+export type Tag = {
+  id: string;
+  label: string;
+};
+
+function App() {
+  const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", []);
+  const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", []);
+
+  return (
+    <Container className="my-4">
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/new" element={<NewNote />} />
+        <Route path="/:id">
+          <Route index element={<h1>show</h1>} />
+          <Route path="edit" element={<h1>edit</h1>} />
+        </Route>
+        <Route path="*" element={<h1>Not found 404 </h1>} />
+      </Routes>
+    </Container>
+  );
+}
+
+export default App;
